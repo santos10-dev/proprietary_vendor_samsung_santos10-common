815517b7f8aa26e521d93feee915242e17e3b808
Benoît Laniel <nels+git@pgroupe.net>
2015-11-01 11:15:25 +0100

    use opensource libva

--- device-common-vendor-blobs.mk
+++ device-common-vendor-blobs.mk
@@ -30,9 +30,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libmultidisplay.so:system/vendor/lib/libmultidisplay.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libsepdrm.so:system/vendor/lib/libsepdrm.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libcorkscrew.so:system/vendor/lib/libcorkscrew.so \
-    vendor/samsung/santos10-common/proprietary/vendor/lib/libva.so:system/vendor/lib/libva.so \
-    vendor/samsung/santos10-common/proprietary/vendor/lib/libva-android.so:system/vendor/lib/libva-android.so \
-    vendor/samsung/santos10-common/proprietary/vendor/lib/libva-tpi.so:system/vendor/lib/libva-tpi.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libpvr2d.so:system/vendor/lib/libpvr2d.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/hw/audio.primary.clovertrail.so:system/vendor/lib/hw/audio.primary.clovertrail.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libaudio-ril.so:system/vendor/lib/libaudio-ril.so \
