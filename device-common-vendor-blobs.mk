720aba7ac7de68dc1ed79a7ec243d8be59059141
Benoît Laniel <nels+git@pgroupe.net>
2016-11-26 09:05:18 +0100

    Revert "add clovertrail keystore"
    This reverts commit 7c8957d82ff7fc013ee862b4c2d71fbd76ced5fc.

--- device-common-vendor-blobs.mk
+++ device-common-vendor-blobs.mk
@@ -44,8 +44,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/santos10-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/samsung/santos10-common/proprietary/vendor/bin/geomagneticd:system/vendor/bin/geomagneticd \
     vendor/samsung/santos10-common/proprietary/vendor/bin/orientationd:system/vendor/bin/orientationd \
-    vendor/samsung/santos10-common/proprietary/vendor/lib/hw/keystore.vendor.clovertrail.so:system/vendor/lib/hw/keystore.vendor.clovertrail.so \
-    vendor/samsung/santos10-common/proprietary/vendor/lib/libsepkeymaster.so:system/vendor/lib/libsepkeymaster.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libcrypto_stock.so:system/vendor/lib/libcrypto_stock.so \
     vendor/samsung/santos10-common/proprietary/vendor/lib/libssl_stock.so:system/vendor/lib/libssl_stock.so \
     vendor/samsung/santos10-common/proprietary/vendor/firmware/fw_sst_08e7.bin:system/vendor/firmware/fw_sst_08e7.bin \
